17a94fcdabc296d1aa3c758c5661ce61286f3b4a
Yogthos <yogthos@theseus.(none)>
2013-04-23 22:09:20 -0400

    figured out how to load processing sketches from string

--- src/cljs/processing/client/editor.cljs
+++ src/cljs/processing/client/editor.cljs
@@ -8,8 +8,16 @@
 
 (defn reload-scene []
   (.log js/console (str "reloading " js/sketchId))
-  (if (not-empty js/sketchId)  
+  (let [code "void setup(){background(100); stroke(10); ellipse(50, 50, 25, 25);}\n"
+        init (js/eval (.-sourceCode (.compile js/Processing code)))
+        processing (new js/Processing (sel1 :#sketch-canvas))]
+    (init processing)
+    (.setup processing))
+  #_(if (not-empty js/sketchId)  
     (load-sketch (sel1 :#sketch-canvas) [(str "/sketch/" js/sketchId)])))
+
+	   
+
 
 (defn save-sketch []
   (ajax/POST "/save-sketch" 
@@ -18,5 +26,5 @@
      :handler reload-scene}))
 
 (defn ^:export init []
-  (reload-scene)
+  (reload-scene) 
   (listen! (dom/by-id "render") :click save-sketch))
