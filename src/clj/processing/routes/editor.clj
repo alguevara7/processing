914bcc396bf1959d27d7905b77f9fb546435359e
Yogthos <yogthos@theseus.(none)>
2013-04-27 11:36:00 -0400

    cleanup

--- src/clj/processing/routes/editor.clj
+++ src/clj/processing/routes/editor.clj
@@ -1,12 +1,12 @@
 (ns processing.routes.editor
-  (:require [compojure.core :refer :all]
+  (:require [ring.util.codec :refer [url-encode]]
+            [compojure.core :refer :all]
             [processing.views.layout :as layout]
             [processing.models.db :as db]
             [noir.session :as session]
             [noir.response :as response]))
 
 (defn editor [& [sketch-id]]
-     
   (session/put! :sketch-id sketch-id)
   (let [{:keys [sketch-id title source-code]} 
         (db/get-sketch-source-code sketch-id)] 
@@ -15,14 +15,18 @@
        :sketch-id sketch-id
        :source-code source-code})))
 
+#_(defn gen-sketch-id [title]
+  (-> (str (session/get :user-id) "-" title)
+      (.replaceAll " " "-")
+      (.toLowerCase)
+      (url-encode)))
+
 (defn save-sketch [title source-code]
-  (session/put!
-    :sketch-id
-    (:sketch-id
-      (db/save-sketch (session/get :user-id) 
-                      (session/get :sketch-id)
-                      title
-                      source-code))))
+  (db/save-sketch 
+    (session/get :user-id) 
+    (session/get :sketch-id)
+    title
+    source-code))
 
 (defroutes editor-routes  
   (GET "/editor" [] (editor))
